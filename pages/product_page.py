ecab9a945fd909f207b221e74b33bb04deb60cb0
Sergey Shershnev <shv.sergey70@gmail.com>
2020-03-08 15:42:52 +0300

    added getter for title in product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,6 +6,9 @@ class ProductPage(BasePage):
     def add_to_basket(self):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
 
+    def get_product_title(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
+
     def get_product_price(self):
         return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
 
@@ -14,8 +17,7 @@ class ProductPage(BasePage):
         assert price_from_alert == correct_price,\
             'price from alert {} is not equals to correct price {}'.format(price_from_alert, correct_price)
 
-    def should_added_item_name_in_notification_equal_to_item_name(self):
-        item_title = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
+    def should_added_item_title_equals(self, correct_title):
         title_from_alert = self.browser.find_element(*ProductPageLocators.ADDED_ITEM_NAME_FROM_ALERT).text
-        assert title_from_alert == item_title, \
-            'name from alert {} is not equals to correct name {}'.format(title_from_alert, item_title)
+        assert title_from_alert == correct_title, \
+            'name from alert {} is not equals to correct title {}'.format(title_from_alert, correct_title)
--- test_product_page.py
+++ test_product_page.py
@@ -2,12 +2,12 @@ from .pages.product_page import ProductPage
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     product_page = ProductPage(browser, link)
     product_page.open()
 
     product_page.add_to_basket()
     product_page.solve_quiz_and_get_code()
 
-    product_page.should_added_item_name_in_notification_equal_to_item_name()
+    product_page.should_added_item_title_equals(product_page.get_product_title())
     product_page.should_basket_price_equals(product_page.get_product_price())
